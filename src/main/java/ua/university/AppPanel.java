47f67b1c7fc32d500fe6136d523c4b4faae1e175
Angelina Shepetiuk <a.shepetiuk@ukma.edu.ua>
2026-05-18 23:04:43 +0300

    draw canvas with frame and stitches

--- src/main/java/ua/university/AppPanel.java
+++ src/main/java/ua/university/AppPanel.java
@@ -1,19 +1,35 @@
 package ua.university;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class AppPanel extends JPanel {
+    private Embroidery embroidery;
+    private Image background;
     public AppPanel() {
-        setPanelSize();
+        setPanel();
+        try {
+            background = ImageIO.read(Objects.requireNonNull(getClass().getResource("/background.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        setUpCanvas();
     }
-    private void setPanelSize() {
+
+    private void setUpCanvas() {
+        embroidery= new Embroidery(30, 25);
+    }
+
+    private void setPanel() {
         Dimension size = new Dimension(EmbroideryCreator.WIDTH, EmbroideryCreator.HEIGHT);
         setPreferredSize(size);
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-    g.drawString("Hello", 100, 100);
+        g.drawImage(background, 0, 0, EmbroideryCreator.WIDTH, EmbroideryCreator.HEIGHT, null);
+        embroidery.draw(g);
     }
 }
---
+++ src/main/java/ua/university/Embroidery.java
@@ -0,0 +1,92 @@
+package ua.university;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Embroidery {
+    public final int MAX_WIDTH = EmbroideryCreator.WIDTH*3/4;
+    public final int MAX_HEIGHT = EmbroideryCreator.HEIGHT*3/4;
+    public final int DEFAULT_STITCH_SIZE=15;
+    public final float BORDER_WIDTH=4;
+    public final Color BEIGE = new Color(250, 240, 230);
+    private int width;
+    private int height;
+    private int stitchSize;
+    private int stitchOffset;
+    private Color[][] canvas;
+    public Embroidery (int width, int height){
+       this.width= width;
+       this.height=height;
+       // if(width*stitchSize>MAX_WIDTH||height*stitchSize>MAX_HEIGHT)
+        stitchSize = width>height? MAX_WIDTH/width: MAX_HEIGHT/height;
+        stitchOffset = (int) (stitchSize*0.2);
+        setupCanvas();
+
+
+    }
+    private void setupCanvas(){
+        canvas = new Color[width][height];
+        for(int i=0; i<width; i++){
+            for(int j=0; j<height; j++){
+                // canvas[i][j] = null;
+                canvas[i][j]=getRandomColor();
+            }
+        }
+    }
+    public void loadFromFile(String path){
+    }
+    public void saveToFile(String name){
+
+    }
+    public void draw(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        int startX=(EmbroideryCreator.WIDTH-width*stitchSize)/2;
+        int startY=(EmbroideryCreator.HEIGHT-height*stitchSize)/2;
+        drawCanvas(startX, startY, g2);
+        drawGrid(startX, startY, g2);
+        drawStitches(startX, startY, g2);
+    }
+
+    private void drawStitches(int startX, int startY,  Graphics2D g2) {
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setStroke(new BasicStroke(4, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        for(int i=0; i<width; i++){
+            for(int j=0; j<height; j++){
+                if(canvas[i][j]!=null)
+                    drawStitch(startX+i*stitchSize, startY+j*stitchSize, stitchSize, canvas[i][j], g2);
+            }
+        }
+    }
+
+    public void drawCanvas(int x, int y, Graphics2D g2){
+        g2.setColor(BEIGE);
+        g2.fillRect(x, y, width*stitchSize, height*stitchSize);
+        g2.setStroke(new BasicStroke(BORDER_WIDTH*2));
+        g2.setColor(Color.BLACK);
+        g2.drawRect((int)(x-BORDER_WIDTH), (int)(y-BORDER_WIDTH),
+                width*stitchSize+(int)BORDER_WIDTH*2, height*stitchSize+(int)BORDER_WIDTH*2);
+    }
+    public void drawGrid(int x, int y, Graphics2D g2){g2.setStroke(new BasicStroke(1.5f));
+       g2.setColor(Color.BLACK);
+        for(int i=0; i<=width; i++)
+            g2.drawLine(x+i*stitchSize, y, x+i*stitchSize, y+height*stitchSize);
+        for(int j=0; j<=height; j++)
+            g2.drawLine(x, y+j*stitchSize, x+width*stitchSize, y+j*stitchSize);
+
+    }
+    public void drawStitch(int x, int y, int size, Color color, Graphics g){
+        g.setColor(color);
+        g.drawLine(x+stitchOffset, y+stitchOffset, x+size-stitchOffset, y+size-stitchOffset);
+        g.drawLine(x+size-stitchOffset, y+stitchOffset, x+stitchOffset, y+size-stitchOffset);
+    }
+    private Color getRandomColor(){
+        return switch (r.nextInt(4)) {
+            case 0 -> Color.RED;
+            case 1 -> Color.BLACK;
+            case 2 -> Color.WHITE;
+            default -> null;
+        };
+
+    }
+    private Random r = new Random();
+}
